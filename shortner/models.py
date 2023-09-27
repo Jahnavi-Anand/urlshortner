fa2c9b490b48c05108c0c8c3a0ebe2fd34e47830
VesperSK <jahnavianandxib@gmail.com>
2023-09-27 16:26:27 +0530

    seting admin pannel and database(sql lite)

--- shortner/models.py
+++ shortner/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 class Url(models.Model):
     link = models.CharField(max_length=10000)
-    uuid = models.CharField(max_length=10000)
+    uuid = models.CharField(max_length=10)
